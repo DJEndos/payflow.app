ecf92398438709786dcf406ea6e720e9516e4209
Endosmile <kolarovmunari@gmail.com>
2026-08-04 22:04:51 +0100

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,9 @@ function renderNav() {
     "afterbegin",
     `<nav class="navbar navbar-expand-lg mb-2">
       <div class="container">
-        <a class="navbar-brand" href="/dashboard.html">PayFlow</a>
+        <a class="navbar-brand" href="/dashboard.html">  <a class="navbar-brand d-flex align-items-center gap-2" href="/index.html">
+       <img src="/logo.png"  width="100">
+      </a></a>
         <div class="ms-auto d-flex gap-2">
           <a href="/dashboard.html" class="btn btn-sm btn-outline-secondary">Dashboard</a>
           <a href="/wallet.html" class="btn btn-sm btn-outline-secondary">Wallet</a>
